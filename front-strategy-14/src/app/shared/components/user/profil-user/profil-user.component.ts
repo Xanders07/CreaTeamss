7037391451fd72b8b93075db6206942db51c6504
Xanders07 <darken2552@gmail.com>
2024-01-19 17:37:37 +0100

    New

--- front-strategy-14/src/app/shared/components/user/profil-user/profil-user.component.ts
+++ front-strategy-14/src/app/shared/components/user/profil-user/profil-user.component.ts
@@ -1,37 +1,49 @@
-// External import
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
-// DTO
-import { UserDataDTO } from '../../../models/user.model';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd, Event as NavigationEvent } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
+import { UserDataDTO } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-profil-user',
   templateUrl: './profil-user.component.html',
   styleUrls: ['./profil-user.component.scss']
 })
-
-export class ProfilUserComponent implements OnInit{
+export class ProfilUserComponent implements OnInit, OnDestroy {
 
   userData: UserDataDTO | null = null;
-  private dataRouteUserSubscription: Subscription | undefined;
+  isSameUser: boolean = true;
+  currentRoute: string = ''; // Variable pour stocker le nom de la route actuelle
 
-  constructor(private activeRoute: ActivatedRoute, private cdr: ChangeDetectorRef) {}
+  private dataRouteUserSubscription: Subscription | undefined;
+  private routerEventsSubscription: Subscription | undefined;
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.dataRouteUserSubscription = this.activeRoute.data.subscribe(({ userData }) => {
       console.log(this.userData);
-
       this.userData = userData;
-      this.cdr.detectChanges();
     });
+
+    // Observer les événements de navigation pour mettre à jour la variable currentRoute
+    this.routerEventsSubscription = this.router.events
+      .subscribe((event: any) => {
+        console.log(event);
+
+        this.currentRoute = event.urlAfterRedirects; // Met à jour le nom de la route
+      });
   }
 
   ngOnDestroy(): void {
     if (this.dataRouteUserSubscription) {
       this.dataRouteUserSubscription.unsubscribe();
     }
-  }
 
+    if (this.routerEventsSubscription) {
+      this.routerEventsSubscription.unsubscribe();
+    }
+  }
 }
